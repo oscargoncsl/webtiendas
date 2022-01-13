e5e9321ed4b8ec14923dce0c5e3158ae730ce9be
borjabp98 <76107400+borjabp98@users.noreply.github.com>
2022-01-13 11:29:52 +0100

    Controllers

--- tiendasTeamRocket/app/Http/Controllers/TiendaController.php
+++ tiendasTeamRocket/app/Http/Controllers/TiendaController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tienda;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Redirect;
 
 class TiendaController extends Controller
 {
@@ -23,7 +28,12 @@ class TiendaController extends Controller
      */
     public function create()
     {
-        //
+          //Controlador accede al modelo para enviarselo a vista 
+        $tiendas = Tienda::all();
+        
+        
+        //Devolvemos la vista
+         return view('tiendas.index',['tiendas'=> $tiendas]);
     }
 
     /**
@@ -34,9 +44,70 @@ class TiendaController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+       //Recibir datos
+        $datos=$request->all();
+       //Validar datos
+       $rules= array (
+        'nombre' => 'required',
+       );
 
+       $messages= array (
+        'nombre.required' => 'Campo nombre es requerido',
+       );
+
+       $validador= Validator::make($datos,$rules,$messages);
+       if($validador->fails()){
+            $errors=$validador->messages();
+            $errors->add('mierror','Se ha cancelado la creación de la tienda.');
+            Session::flash('tipoMensaje','danger');
+            Session::flash('mensaje','Error, no se cumplen las validaciones. Compruebe todos los campos');
+            //Volver con los errores
+            
+            return Redirect::back()->withInput()->withErrors($validador);
+        }else{
+
+                //Generar plotter
+                $tienda=new Tienda();
+                $plotter->marca=$datos["marca"];
+                $plotter->modelo=$datos["modelo"];
+                $plotter->nombre=$datos["nombre"];
+                $plotter->descripcion=$datos["descripcion"];
+                $plotter->velocidad=$datos["velocidad"];
+                
+            
+            //Generar un nombre único para la imagen
+            $nombreImagen=Str::random(30)."-".$request->file('imagen')->getClientOriginalName();
+            //Asociarselo al modelo
+            $plotter->imagen=$nombreImagen;
+            try{
+                //Almacenar en la BD 
+                $tienda->save();
+                //Almacenar el archivo en el servidor
+                $request->file('imagen')->move('images/plotters',$nombreImagen);   
+                    //Volver al listado
+                    //Mensaje de OK
+                    Session::flash('tipoMensaje','success');
+                    Session::flash('mensaje','Plotter creado correctamente');
+                
+            }catch(\Exception $e){
+                //echo $e->getMessage();
+                //Mensaje de KO
+                Session::flash('tipoMensaje','danger');
+                Session::flash('mensaje','Error al crear el plotter');
+
+
+            }
+            return Redirect::back();
+        }
+
+
+
+        
+
+
+
+
+    }
     /**
      * Display the specified resource.
      *
@@ -45,7 +116,14 @@ class TiendaController extends Controller
      */
     public function show($id)
     {
-        //
+        $tienda=Tienda::find($id);
+       if (is_null($tienda))
+        echo "No existe el plotter solicitado";    
+       else
+       {
+        //Devolvemos la vista
+        return view('tiendas.show',['tienda'=> $tienda]);
+       }
     }
 
     /**
@@ -77,8 +155,11 @@ class TiendaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Tienda $tienda)
     {
-        //
+        $tienda->delete();
+        Session::flash('tipoMensaje','info');
+        Session::flash('mensaje','Plotter borrado correctamente');
+        return Redirect::back();
     }
 }
