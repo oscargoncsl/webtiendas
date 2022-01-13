84ef679a17802882f8cabed9b1b8a19702da32bc
borjabp98 <76107400+borjabp98@users.noreply.github.com>
2022-01-13 11:56:10 +0100

    Migrate Solucionado

--- tiendasTeamRocket/database/migrations/2022_01_13_095543_create_productos_table.php
+++ tiendasTeamRocket/database/migrations/2022_01_13_095543_create_productos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductoTable extends Migration
+class CreateProductosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -20,7 +20,7 @@ class CreateProductoTable extends Migration
             $table->double('precio');
             $table->string('imagen');
             $table->bigInteger('tienda_id')->unsigned(); //No negativos y mayor rango positivo
-            $table->foreign('tienda_id')->references('id')->on('tienda');
+            $table->foreign('tienda_id')->references('id')->on('tiendas');
             $table->timestamps();
         });
     }
