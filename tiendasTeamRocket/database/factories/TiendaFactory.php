e166da386e40708461ccb3eb15f4dda83c1fadcf
borjabp98 <76107400+borjabp98@users.noreply.github.com>
2022-01-13 21:31:00 +0100

    Factory

--- tiendasTeamRocket/database/factories/TiendaFactory.php
+++ tiendasTeamRocket/database/factories/TiendaFactory.php
@@ -1,19 +1,18 @@
- <?php
+<?php
 
 namespace Database\Factories;
 
-use App\Models\User;
+use App\Models\Tienda;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
-class UserFactory extends Factory
+class TiendaFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = User::class;
+    protected $model = Tienda::class;
 
     /**
      * Define the model's default state.
@@ -28,6 +27,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'role_id' => 1
         ];
     }
 }
