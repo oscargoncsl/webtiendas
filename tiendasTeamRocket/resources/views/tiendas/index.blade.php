8f68026923be5737233e129223db407c3d15965a
oscargoncsl <76107521+oscargoncsl@users.noreply.github.com>
2022-01-13 12:19:01 +0100

    añadido form tienda

--- tiendasTeamRocket/resources/views/tiendas/index.blade.php
+++ tiendasTeamRocket/resources/views/tiendas/index.blade.php
@@ -7,7 +7,7 @@
 <h2>Tiendas   <!-- Button trigger modal -->
     <!-- Button trigger modal -->
   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Añadir
+    Añadir comercio
   </button>
 </h2>
 
@@ -55,49 +55,28 @@
     <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Creación de un nuevo plotter</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Añadir nuevo comercio</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
+
 
           <div class="col-md-6">
             <label for="nombre" class="form-label">Nombre</label>
             <input type="text" class="form-control" id="nombre" name="nombre">
           </div>
-          <div class="col-md-6">
-            <label for="marca" class="form-label">Marca</label>
-            <input type="text" class="form-control" id="marca" name="marca">
-          </div>
+
           <div class="col-12">
-            <label for="modelo" class="form-label">Modelo</label>
-            <input type="text" class="form-control" id="modelo" name="modelo" placeholder="Modelo de plotter, no de lencería">
+            <label for="ubicacion" class="form-label">Ubicación</label>
+            <input type="text" class="form-control" id="ubicacion" placeholder="Autonomia 32">
           </div>
-          <div class="col-12">
-            <label for="descripcion" class="form-label">Descripcion</label>
-            <input type="text" class="form-control" id="descripcion" name="descripcion" placeholder="Bla bla bla....">
-          </div>
-          <div class="col-12">
-            <label for="velocidad" class="form-label">Velocidad</label>
-            <input type="number" class="form-control" id="velocidad" name="velocidad" placeholder="¿300.000?">
-          </div>
-          <div class="mb-3">
-            <label for="imagen" class="form-label">Foto</label>
-            <input class="form-control" type="file" id="imagen" name="imagen">
-          </div>
-          <div class="col-12">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="gridCheck">
-              <label class="form-check-label" for="gridCheck">
-               Confirma que aceptas que nos quedemos con toda tu información
-              </label>
-            </div>
-          </div>
+          
 
 
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-        <button type="submit" class="btn btn-primary">Crear</button>
+        <button type="submit" class="btn btn-primary">Añadir comercio</button>
       </div>
 
     </div>
